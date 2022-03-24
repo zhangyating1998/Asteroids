a5a6ed7b69b82b77fc916f04f55317bced856529
Kingsley <long.kinglsey@gmail.com>
2022-03-24 17:47:06 +0000

    1. add testing code 2. add interfaces and basic classes

---
+++ src/group13/application/common/GameLevel.java
@@ -0,0 +1,5 @@
+package group13.application;
+
+public enum GameLevel {
+    LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4, LEVEL_5
+}
---
+++ src/group13/application/common/ShipType.java
@@ -0,0 +1,5 @@
+package group13.application;
+
+public enum ShipType {
+    PLAYER, ALIEN
+}
