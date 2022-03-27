d90771f7a614ced17612088d955cc7f30fffd829
zhang yating <70979812+Zhang-yating@users.noreply.github.com>
2022-03-27 13:46:02 +0100

    constructor implemented

--- src/group13/application/asteroid/SmallAsteroid.java
+++ src/group13/application/asteroid/SmallAsteroid.java
@@ -1,4 +1,13 @@
 package group13.application.asteroid;
 
+import javafx.scene.Group;
+
 public class SmallAsteroid extends Asteroid {
+    Double[] coors;
+    double speed;
+    public SmallAsteroid(){
+        super(new Double[]{8.0, 0.0, 15.0, 3.0, 13.0, 13.0, 9.0, 15.0, 0.0, 10.0});
+        this.speed = 300;
+    }
+
 }
