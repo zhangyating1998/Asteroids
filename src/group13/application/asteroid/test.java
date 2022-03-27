57b07d85d703cb06fa6c29e2d1e36ab2c1663456
zhang yating <70979812+Zhang-yating@users.noreply.github.com>
2022-03-27 13:47:26 +0100

    a test file created three moving asteroids

---
+++ src/group13/application/asteroid/test.java
@@ -0,0 +1,34 @@
+package group13.application.asteroid;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class test extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Group root = new Group();
+
+        LargeAsteroid largeAs = new LargeAsteroid();
+        largeAs.Translate(0,0,800, 600, 80);
+
+        MediumAsteroid mediAs = new MediumAsteroid();
+        mediAs.Translate(100, 0, 800, 400, 100);
+
+        SmallAsteroid smallAs = new SmallAsteroid();
+        smallAs.Translate(0, 300, 500, 0, 200 );
+
+        root.getChildren().addAll(mediAs.asteroid, largeAs.asteroid, smallAs.asteroid);
+        Scene scene = new Scene(root, 800, 600, Color.BLACK);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
