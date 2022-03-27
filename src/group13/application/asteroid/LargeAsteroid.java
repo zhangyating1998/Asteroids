9f90829e1d12dc0a2b9f3fb9421b3efa177802a5
zhang yating <70979812+Zhang-yating@users.noreply.github.com>
2022-03-27 13:44:47 +0100

    largeAsteroids

--- src/group13/application/asteroid/LargeAsteroid.java
+++ src/group13/application/asteroid/LargeAsteroid.java
@@ -1,4 +1,19 @@
 package group13.application.asteroid;
 
+import javafx.scene.Group;
+
 public class LargeAsteroid extends Asteroid {
+    Double[] coors ;
+    double speed;
+    public LargeAsteroid(){
+        super(new Double[]{20.0, 0.0, 60.0, 40.0, 35.0, 60.0, 0.0,45.0, 4.0, 20.0});
+        this.speed = 80;
+    }
+
+    public LargeAsteroid(Double[] coors){
+        super(coors);
+        this.coors = coors;
+
+    }
+
 }
