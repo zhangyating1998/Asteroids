74459d53c0f48f72f9dd5220370120dd88de55df
zhang yating <70979812+Zhang-yating@users.noreply.github.com>
2022-04-01 22:16:24 +0100

    can create asteroid of random shape and initial position, finish split method

---
+++ asteroid/SmallAsteroid.java
@@ -0,0 +1,11 @@
+package group13.application.characters.asteroid;
+
+public class SmallAsteroid extends Asteroid {
+    Double[] coors;
+    double speed;
+    public SmallAsteroid(){
+        super();
+        this.speed = 300;
+    }
+
+}
