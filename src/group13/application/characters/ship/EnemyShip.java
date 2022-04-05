70cc55dac32010f5810a0245ed6dd2b1c121d51c
LUCSE20 <90796221+LUCSE20@users.noreply.github.com>
2022-04-05 15:13:28 +0100

    Alien Ship moves

--- src/group13/application/characters/ship/EnemyShip.java
+++ src/group13/application/characters/ship/EnemyShip.java
@@ -1,11 +1,38 @@
 package group13.application.characters.ship;
 
-/**
- * @author longyu
- * @date 2022/3/26 15:22
- */
+import javafx.geometry.Point2D;
+import java.util.Random;
+
+
 public class EnemyShip extends Ship {
+    private Point2D AlienSpeed;
+
+
     public EnemyShip(int x, int y) {
-        super(x, y, -15, -15, 30, 0, -15, 15);
+        super(x, y, -10, -10, -20, 20, 10, 10, 20, -20);
+
+
+        Random rnd = new Random();
+
+        super.setRotate(rnd.nextInt(360));
+        this.AlienSpeed = new Point2D(0, 0);
+        AlienPropulsion();
+
     }
-}
+    public void move() {
+        setTranslateX(getTranslateX() + this.AlienSpeed.getX());
+        setTranslateY(getTranslateY() + this.AlienSpeed.getY());
+    }
+    public void AlienPropulsion() {
+        double changeX = Math.cos(Math.toRadians(getRotate()));
+        double changeY = Math.sin(Math.toRadians(getRotate()));
+
+        changeX *= 0.05;
+        changeY *= 0.05;
+
+        this.AlienSpeed = this.AlienSpeed.add(changeX, changeY);
+
+
+    }}
+
+
