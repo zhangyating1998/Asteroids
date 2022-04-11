9a887c581956b75dd9364926189cd2775525f038
Sean <schristopher.lucas@gmail.com>
2022-04-11 16:23:01 +0100

    psuedo code added to class

--- src/group13/application/characters/ship/EnemyShip.java
+++ src/group13/application/characters/ship/EnemyShip.java
@@ -1,5 +1,6 @@
 package group13.application.characters.ship;
 
+import group13.application.common.Constants;
 import javafx.geometry.Point2D;
 import java.util.Random;
 
@@ -11,18 +12,52 @@ public class EnemyShip extends Ship {
     public EnemyShip(int x, int y) {
         super(x, y, -10, -10, -20, 20, 10, 10, 20, -20);
 
-
+        // 1. generate random x, y
+        // 1.1 x = 0 or WIDTH
+        // 1.2 y = random ( HEIGHT )
         Random rnd = new Random();
 
-        super.setRotate(rnd.nextInt(360));
-        this.AlienSpeed = new Point2D(0, 0);
-        AlienPropulsion();
+        // 2. change the angle based on x
+//        super.setRotate(rnd.nextInt(360));
+        //speed can be adjusted for game Level
+        this.AlienSpeed = new Point2D(1, 2);
+       // AlienPropulsion();
+
+
 
     }
+
+    @Override
+    public void fire() {
+        super.fire();
+        // 1. create Bullet instance
+        // 2. fire an event, pass in the instance
+    }
+
     public void move() {
+        // 1. check if it reaches the edge
+        // 2. change the speed of the ship
         setTranslateX(getTranslateX() + this.AlienSpeed.getX());
         setTranslateY(getTranslateY() + this.AlienSpeed.getY());
-    }
+
+        //set visable
+//        if (this.getTranslateX() + this.getLayoutBounds().getWidth() < 0)
+//
+        // 2. fire an event
+        // print('...')
+//            //add eventlistener
+//            this.setTranslateX(Constants.SCENE_WIDTH);
+//
+//        if (this.getTranslateX() > Constants.SCENE_WIDTH)
+//            this.setTranslateX(-this.getLayoutBounds().getWidth());
+//
+//        if (this.getTranslateY() + this.getLayoutBounds().getHeight() < 0)
+//            this.setTranslateY(Constants.SCENE_HEIGHT);
+//
+//        if (this.getTranslateY() > Constants.SCENE_HEIGHT)
+//            this.setTranslateY(-this.getLayoutBounds().getHeight());
+            }
+
     public void AlienPropulsion() {
         double changeX = Math.cos(Math.toRadians(getRotate()));
         double changeY = Math.sin(Math.toRadians(getRotate()));
