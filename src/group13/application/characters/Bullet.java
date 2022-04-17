bfc6b239b0014ea65d1f274113a8c0f20fbd8215
Aonghus-B <91098222+Aonghus-B@users.noreply.github.com>
2022-04-17 12:58:40 +0100

    Integrated bullet class for playership

--- src/group13/application/characters/Bullet.java
+++ src/group13/application/characters/Bullet.java
@@ -2,8 +2,9 @@ package group13.application.characters;
 
 public class Bullet extends Character {
 
+    // Create a bullet based on the character class, includes time to live limit
     public Bullet(int x, int y) {
-        super(x, y, true, 2.2, 5, -5, 5, 5, -5, 5, -5, -5);
+        super(x, y, true, 2.2, 2.5, -2.5, 2.5, 2.5, -2.5, 2.5, -2.5, -2.5);
     }
     }
 
